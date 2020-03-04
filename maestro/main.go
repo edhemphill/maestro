71e0824a2c88832acb8dba4283dbb1ac7fb97ef7
J. Michael Welsh <mike.welsh@arm.com>
2020-03-04 06:16:58 +0000

    Change Hooks (Work in progress)
    Adding hooks to handle changes to log filters.

--- maestro/main.go
+++ maestro/main.go
@@ -19,9 +19,15 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"os/signal"
 	"strings"
 	"sync"
+	"syscall"
+
 	//	"reflect"
+	"net/http"
+	"time"
+
 	"github.com/armPelionEdge/greasego"
 	"github.com/armPelionEdge/httprouter"
 	. "github.com/armPelionEdge/maestro"
@@ -43,8 +49,7 @@ import (
 	"github.com/armPelionEdge/maestroSpecs"
 	"github.com/armPelionEdge/maestroSpecs/templates"
 	"github.com/op/go-logging"
-	"net/http"
-	"time"
+
 	// Platforms
 	"github.com/armPelionEdge/maestro/platforms"
 	// platform_rp200 "github.com/armPelionEdge/maestro/platforms/rp200"
@@ -76,6 +81,29 @@ func main() {
 	}
 
 	log.Info("maestro starting.")
+
+	// Debug function that runs on USR1 signal
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGUSR1)
+		for {
+			<-c
+			fmt.Println("[*] SIGUSR1 received")
+			// add all types of log messages here
+			fileLog.Info("debug log output - Info")
+			fileLog.Warn("debug log output - Warn")
+			fileLog.Error("debug log output - Error")
+			fileLog.Debug("debug log output - Debug")
+			fileLog.Success("debug log output - Success")
+
+			fmt.Println("    maestro version: maestroutils.Version()")
+			fmt.Println("    meta vars: {{VARNAME}} = [[VALUE]]")
+			dict := maestroConfig.GetGlobalConfigDictionary()
+			for varname, val := range dict.Map {
+				fmt.Printf("        {{%s}} = [[%s]]\n", varname, val)
+			}
+		}
+	}()
 
 	configFlag := flag.String("config", "./maestro.config", "Config path")
 	dumpMetaVars := flag.Bool("dump_meta_vars", false, "Dump config file meta variables only")
@@ -555,7 +583,6 @@ func main() {
 	} else {
 		go bringUpIfs()
 	}
-
 
 	/*********************************************/
 	/*               Jobs startup                */
